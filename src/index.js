e0883073f94bf3ecc9652d4c3b35b0ab5601935d
kazma <kazma.chihaya@arrow-payment.com>
2021-09-17 20:51:15 +0900

    refactoring

--- src/index.js
+++ src/index.js
@@ -1,87 +1,68 @@
 var drag_type = null;
 
+// 位置取得
+function clickPosition(e) {
+  return {x: e.clientX, y: e.clientY};
+}
+function touchPosition(e) {
+  const touch = e.changedTouches[0];
+  return {x: touch.pageX, y: touch.pageY};
+}
+
 // ドラッグ開始
 $('.dragdrop_origin').on('mousedown', function(e) {
-  var drag_origin_element = document.elementFromPoint(e.clientX, e.clientY);
-  if (drag_origin_element !== null && drag_origin_element.classList.contains('dragdrop_origin')) {
-    console.log('mousedown');
-    e.preventDefault();
-    drag_type = drag_origin_element.dataset.drag_type;
-    startDrag(drag_origin_element.dataset);
-  }
+  startDragSource(e, clickPosition(e));
 });
 $('.dragdrop_origin').on('touchstart', function(e) {
-  var drag_origin_element = document.elementFromPoint(touch.pageX, touch.pageY);
+  startDragSource(e, touchPosition(e));
+});
+function startDragSource(e, offset) {
+  var drag_origin_element = document.elementFromPoint(offset.x, offset.y);
   if (drag_origin_element !== null && drag_origin_element.classList.contains('dragdrop_origin')) {
-    console.log('touchstart');
     e.preventDefault();
     drag_type = drag_origin_element.dataset.drag_type;
     startDrag(drag_origin_element.dataset);
   }
-});
+}
 
 
 // ドラッグ中
 $(document).on('mousemove', function(e) {
-  if (drag_type !== null) {
-    var dragging = $('.dragging.' + drag_type)[0];
-    if (dragging) {
-      console.log('mousemove');
-      var parent_offset = $(dragging).parent().offset();
-      var offset_x_plus = dragging.dataset.offset_x_plus ?? 0;
-      var offset_x_minus = (dragging.dataset.offset_x_minus ?? 0) + parent_offset.left;
-      var offset_y_plus = dragging.dataset.offset_y_plus ?? 0;
-      var offset_y_minus = dragging.dataset.offset_y_minus ?? 0;
-
-      dragging.style.display = 'block';
-      dragging.style.transform = 'translate(calc(calc(calc(' + e.clientX + 'px + ' + window.pageXOffset + 'px) + ' + offset_x_plus + 'px) - ' + offset_x_minus + 'px), calc(calc(calc(' + e.clientY + 'px + ' + window.pageYOffset + 'px) + ' + offset_y_plus + 'px) - ' + offset_y_minus + 'px))';
-    }
-  }
+  draggingSource(e, clickPosition(e));
 });
 $(document).on('touchmove', function(e) {
+  draggingSource(e, touchPosition(e));
+});
+function draggingSource(e, offset) {
   if (drag_type !== null) {
     var dragging = $('.dragging.' + drag_type)[0];
     if (dragging) {
-      console.log('touchmove');
       var parent_offset = $(dragging).parent().offset();
       var offset_x_plus = dragging.dataset.offset_x_plus ?? 0;
       var offset_x_minus = (dragging.dataset.offset_x_minus ?? 0) + parent_offset.left;
       var offset_y_plus = dragging.dataset.offset_y_plus ?? 0;
       var offset_y_minus = dragging.dataset.offset_y_minus ?? 0;
 
-      const touch = e.changedTouches[0];
       dragging.style.display = 'block';
-      dragging.style.transform = 'translate(calc(calc(calc(' + touch.pageX + 'px + ' + window.pageXOffset + 'px) + ' + offset_x_plus + 'px) - ' + offset_x_minus + 'px), calc(calc(calc(' + touch.pageY + 'px + ' + window.pageYOffset + 'px) + ' + offset_y_plus + 'px) - ' + offset_y_minus + 'px))';
+      dragging.style.transform = 'translate(calc(calc(calc(' + offset.x + 'px + ' + window.pageXOffset + 'px) + ' + offset_x_plus + 'px) - ' + offset_x_minus + 'px), calc(calc(calc(' + offset.y + 'px + ' + window.pageYOffset + 'px) + ' + offset_y_plus + 'px) - ' + offset_y_minus + 'px))';
     }
   }
-});
+}
 
 
 // ドラッグ終了
 $(document).on('mouseup', function(e) {
-  if (drag_type !== null) {
-    var drop_point_element = document.elementFromPoint(e.clientX, e.clientY);
-    if (drop_point_element.classList.contains('dragdrop_dest') && drop_point_element.classList.contains(drag_type)) {
-      console.log('mouseup');
-      endDrag(drop_point_element.dataset);
-    }
-  }
-  cancelDragSource();
+  endDragSource(e, clickPosition(e));
 });
 $(document).on('touchend', function(e) {
+  endDragSource(e, touchPosition(e));
+});
+function endDragSource(e, offset) {
   if (drag_type !== null) {
-    const touch = e.changedTouches[0];
-    var drop_point_element = document.elementFromPoint(touch.pageX, touch.pageY);
+    var drop_point_element = document.elementFromPoint(offset.x, offset.y);
     if (drop_point_element.classList.contains('dragdrop_dest') && drop_point_element.classList.contains(drag_type)) {
-      console.log('touchend');
       endDrag(drop_point_element.dataset);
     }
-  }
-  cancelDragSource();
-});
-
-function cancelDragSource() {
-  if (drag_type !== null) {
     var dragging = $('.dragging.' + drag_type)[0];
     if (dragging) {
       dragging.style.display = 'none';

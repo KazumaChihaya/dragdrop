0e3cc68f82899f75e2e67631e3471521cbc10e14
kazma <kazma.chihaya@arrow-payment.com>
2021-09-19 05:27:22 +0900

    fix library

--- README.md
+++ README.md
@@ -3,11 +3,28 @@
 インポート方法
 <script type="module" src="dragdropのindex.jsへのパス"></script>
 
-ドラッグされるやつ .dragdrop_origin .タグ
+または、vueなどの場合、
+import dragdrop from 'dragdrop'　とする(名前だけ書くと、node_modules内のものを探しに行くらしい)
+
+created() {
+  dragdrop(this);
+}
+を追加する。下記の呼び出しもとの関数を含むオブジェクトを渡すことで、イベント発生時にその関数を呼ぶ。
+
+$(document).on('click', '.class', function() {})
+としたことで、動的に追加した要素にもclickイベントが発生するようになっている
+
+タグはdata-drag_type=""としてかく
+
+ドラッグされるやつ .dragdrop_origin data-drag_type="タグ"
 ドラッグされてるやつ .dragging .タグ
 ドロップされる場所 .dragdrop_dest .タグ
 全てに、data-drag_type="タグ"　をつける
 「タグ」は、種類を示すタグ　複数の種類のドラッグドロップを使用する際に区別できるようにつける
+
+draggingに対して、
+data-offset_x_plus="0" data-offset_x_minus="0" data-offset_y_plus="0" data-offset_y_minus="0"
+を入れる
 
 CSS
 .draggingのdivへのCSS
--- src/index.js
+++ src/index.js
@@ -1,4 +1,43 @@
 var drag_type = null;
+var self = null;
+
+export default function dragdrop(_this) {
+  self = _this;
+
+  // ドラッグ開始
+  $(document).on('mousedown', '.dragdrop_origin', function(e) {
+    e.preventDefault();
+    startDragSource(e, clickPosition(e));
+  });
+  $(document).on('touchstart', '.dragdrop_origin', function(e) {
+    e.preventDefault();
+    startDragSource(e, touchPosition(e));
+  });
+
+
+
+  // ドラッグ中
+  document.addEventListener('mousemove', function(e) {
+    if (drag_type !== null) {
+      e.preventDefault();
+      draggingSource(e, clickPosition(e));
+    }
+  });
+  document.addEventListener('touchmove', function(e) {
+    if (drag_type !== null) {
+      e.preventDefault();
+      draggingSource(e, touchPosition(e));
+    }
+  }, {passive: false});
+
+  // ドラッグ終了
+  $(document).on('mouseup', function(e) {
+    endDragSource(e, clickPosition(e));
+  });
+  $(document).on('touchend', function(e) {
+    endDragSource(e, touchPosition(e));
+  });
+}
 
 // 位置取得
 function clickPosition(e) {
@@ -9,41 +48,26 @@ function touchPosition(e) {
   return {x: touch.pageX - window.pageXOffset, y: touch.pageY - window.pageYOffset};
 }
 
-// ドラッグ開始
-$('.dragdrop_origin').on('mousedown', function(e) {
-  e.preventDefault();
-  startDragSource(e, clickPosition(e));
-});
-$('.dragdrop_origin').on('touchstart', function(e) {
-  e.preventDefault();
-  startDragSource(e, touchPosition(e));
-});
+
+
+
 function startDragSource(e, offset) {
-  var drag_origin_element = document.elementFromPoint(offset.x, offset.y);
+  var drag_origin_element = e.currentTarget;
   if (drag_origin_element !== null && drag_origin_element.classList.contains('dragdrop_origin')) {
     e.preventDefault();
     drag_type = drag_origin_element.dataset.drag_type;
-    startDrag(drag_origin_element.dataset);
+    self.startDrag(drag_origin_element.dataset);
   }
 }
-
-
-// ドラッグ中
-$(document).on('mousemove', function(e) {
-  draggingSource(e, clickPosition(e));
-});
-$(document).on('touchmove', function(e) {
-  draggingSource(e, touchPosition(e));
-});
 function draggingSource(e, offset) {
   if (drag_type !== null) {
     var dragging = $('.dragging.' + drag_type)[0];
     if (dragging) {
       var parent_offset = $(dragging).parent().offset();
-      var offset_x_plus = dragging.dataset.offset_x_plus ?? 0;
-      var offset_x_minus = (dragging.dataset.offset_x_minus ?? 0) + parent_offset.left;
-      var offset_y_plus = dragging.dataset.offset_y_plus ?? 0;
-      var offset_y_minus = dragging.dataset.offset_y_minus ?? 0;
+      var offset_x_plus = Number(dragging.dataset.offset_x_plus);
+      var offset_x_minus = Number(dragging.dataset.offset_x_minus) + parent_offset.left;
+      var offset_y_plus = Number(dragging.dataset.offset_y_plus);
+      var offset_y_minus = Number(dragging.dataset.offset_y_minus) + parent_offset.top;
 
       dragging.style.display = 'block';
       dragging.style.transform = 'translate(calc(calc(calc(' + offset.x + 'px + ' + window.pageXOffset + 'px) + ' + offset_x_plus + 'px) - ' + offset_x_minus + 'px), calc(calc(calc(' + offset.y + 'px + ' + window.pageYOffset + 'px) + ' + offset_y_plus + 'px) - ' + offset_y_minus + 'px))';
@@ -51,19 +75,11 @@ function draggingSource(e, offset) {
   }
 }
 
-
-// ドラッグ終了
-$(document).on('mouseup', function(e) {
-  endDragSource(e, clickPosition(e));
-});
-$(document).on('touchend', function(e) {
-  endDragSource(e, touchPosition(e));
-});
 function endDragSource(e, offset) {
   if (drag_type !== null) {
     var drop_point_element = document.elementFromPoint(offset.x, offset.y);
     if (drop_point_element.classList.contains('dragdrop_dest') && drop_point_element.classList.contains(drag_type)) {
-      endDrag(drop_point_element.dataset);
+      self.endDrag(drop_point_element.dataset);
     }
     var dragging = $('.dragging.' + drag_type)[0];
     if (dragging) {
@@ -71,5 +87,6 @@ function endDragSource(e, offset) {
     }
     drag_type = null;
   }
-  cancelDrag();
+  self.cancelDrag();
 }
+
